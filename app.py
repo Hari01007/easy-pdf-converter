58ab519155a013956e07cb2966216fccc43d0949
Hari01007 <166573247+Hari01007@users.noreply.github.com>
2025-12-26 01:01:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,7 @@ def jpg_to_pdf():
     return send_file(out_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
